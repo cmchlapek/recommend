8bc3bc1d4c04bb34b010b8d5d83283562fdc43fd
Casey <cmchlapek@gmail.com>
2021-07-25 11:50:33 -0500

    Node class completed.

---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+    
+    def get_next_node(self):
+        return self.value
